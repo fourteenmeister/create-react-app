4fb6803817c1f454b422b3fd05fd6bc92ce1fdbf
CodeBreaker <andrey.kamaev@its-trade.com>
2019-06-06 17:08:40 +0300

    Modify template-typescript

---
+++ packages/react-scripts/template-typescript/src/containers/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const App: React.FC = (): React.ReactElement => {
+  return (
+    <div>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </div>
+  );
+};
+
+export default App;
--- packages/react-scripts/template-typescript/src/index.tsx
+++ packages/react-scripts/template-typescript/src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from 'containers/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'index.css';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
+
+if (module.hot) {
+  module.hot.accept(
+    'containers/App',
+    (): void => {
+      const NextApp = require('containers/App').default;
+      ReactDOM.render(<NextApp />, rootEl);
+    }
+  );
+}
